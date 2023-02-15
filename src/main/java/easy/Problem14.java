5701a8280f03ca95569868085d08f70c38a0d45b
duyenthang-dev <duyenthang.dev@gmail.com>
2023-02-15 10:07:59 +0700

    initial commit

---
+++ src/main/java/easy/Problem14.java
@@ -0,0 +1,28 @@
+package easy;
+
+import java.util.Arrays;
+
+public class Problem14 {
+    // ý tưởng: chỉ quan tâm chuỗi dài nhất và ngắn nhất
+    // ví dụ: {"ab", "a", "d"} -> {"a", "ab", "d"}
+    // ta chỉ quan tâm "a" và "d",
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+
+    public static void main(String[] args) {
+        String[] temp = new String[]{"ab", "a", "d"};
+        System.out.println(new Problem14().longestCommonPrefix(temp));
+    }
+}
---
+++ src/main/java/easy/Problem989.java
@@ -0,0 +1,15 @@
+package easy;
+
+import java.util.List;
+
+public class Problem989 {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {2,7,4};
+        int k = 181;
+        System.out.println(new Problem989().addToArrayForm(arr, k));
+    }
+}

19cddfa6d7d60a889f540add088b09f2f1c7b33e
Ha Duyen Thang <duyenthang.dev@gmail.com>
2024-02-01 22:37:32 +0700

    solved problem 151

---
+++ src/main/java/medium/ZigzagConversion_6.java
@@ -0,0 +1,14 @@
+package medium;
+
+public class ZigzagConversion_6 {
+    public String convert(String s, int numRows) {
+        StringBuilder sb = new StringBuilder();
+
+        return null;
+    }
+    public static void main(String[] args) {
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        System.out.println(new ZigzagConversion_6().convert(s, numRows));
+    }
+}

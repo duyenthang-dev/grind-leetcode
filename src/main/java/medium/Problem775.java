b18ea6e062e63ec2dabb7abc82983cc6cdb8534f
Duyen Thang <duyenthang.dev@gmail.com>
2024-06-12 14:20:30 +0700

    done for Apr 24

--- src/main/java/medium/Problem775.java
+++ src/main/java/medium/Problem775.java
@@ -1,2 +1,11 @@
-package medium;public class Problem775 {
+package medium;
+
+public class Problem775 {
+    public boolean isIdealPermutation(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (Math.abs(nums[i] - i) > 1)
+                return false;
+        }
+        return true;
+    }
 }
--- src/main/java/medium/SpiralMatrix_54.java
+++ src/main/java/medium/SpiralMatrix_54.java
@@ -1,2 +1,29 @@
-package medium;public class SpiralMatrix_54 {
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Runtime: 2ms - beat 80% java user
+ * Memory: 44mb - beat 85%% java user
+ */
+public class SpiralMatrix_54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int i =0; int j = 0;
+        int minRow =0, maxRow = matrix.length;
+        int minCol = 0, maxCol = matrix[0].length;
+        var  res = new ArrayList<Integer>();
+        int total = matrix.length * matrix[0].length;
+        while (total > 0) {
+            res.add(matrix[i][j]);
+//            if
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]
+                {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        System.out.println(new SpiralMatrix_54().spiralOrder(matrix));
+    }
 }
